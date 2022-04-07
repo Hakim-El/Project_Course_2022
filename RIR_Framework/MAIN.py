bdddf0521c70495666ebf2d7192d4b26c944e478
Lorenzo <81576481+LorenzoLellini@users.noreply.github.com>
2022-04-07 15:24:34 +0200

    completamento main

--- RIR_Framework/MAIN.py
+++ RIR_Framework/MAIN.py
@@ -1,21 +1,8 @@
-from turtle import clear
-import numpy as np
-import os
-from matplotlib import pyplot as plt
-from scipy.signal import find_peaks
-from scipy.optimize import minimize
-from scipy import interpolate
-from scipy.signal import spectrogram
-from scipy.io.wavfile import write as wavwrite
 import sounddevice as sd
-
-# modules from Sine Sweep measure
-# import stimulus_SineSweep as stim
-# import _parseargs_SineSweep as parse
-# import utils_SineSweep as utils
-
-# modules from Calibration
-#import Calibration as cal
+import numpy as np
+import matplotlib
+import scipy
+import os
 
 ######### INFORMAZIONI SULLA MISURA DA CHIEDERE ALL'UTENTE
 
@@ -54,6 +41,7 @@ elif fs_selection != 1 and fs_selection!= 2 and fs_selection != 3 :
         fs = 96000
 
 print("\nVuoi fare una calibrazione in 2D o in 3D?\n \n- Digita 1 per 2D\n- Digita 2 per 3D\n")
+print("NB: Per una calibrazione in 2D servono almeno 3 sorgenti per determinare la posizione dei microfoni.\nPer una calibrazione in 3D servono almeno 4 sorgenti per determinare la posizione dei microfoni.")
 cal_type = int(input())
 
 if cal_type != 1 and cal_type != 2 :
@@ -61,15 +49,55 @@ if cal_type != 1 and cal_type != 2 :
     print("\nVuoi fare una calibrazione in 2D o in 3D?\n \n- Digita 1 per 2D\n- Digita 2 per 3D\n")
     cal_type = int(input())
 
+if cal_type == 1 :
+    print("\nValore asse X (Lunghezza) della stanza [m]:")
+    x_axis = float(input())
+    print("\nValore asse Y (Larghezza) della stanza [m]:")
+    y_axis = float(input())
+elif cal_type == 2 :
+    print("\nValore asse X (Lunghezza) della stanza [m]:")
+    x_axis = float(input())
+    print("\nValore asse Y (Larghezza) della stanza [m]:")
+    y_axis = float(input())
+    print("\nValore asse Z (Altezza) della stanza [m]:")
+    z_axis = float(input())
+
+print("\nVuoi tenere in considerazione il delay di elaborazione dell'algoritmo nel calcolo della RIR e della calibrazione?")
+print("\n- 1 -> SI\n- 2 -> NO")
+delayType = int(input()) # = 1 delay, = 2 NO delay
+
+if delayType != 1 and delayType != 2 :
+    print("\nERRORE!\nVuoi tenere in considerazione il delay di elaborazione dell'algoritmo nel calcolo della RIR e della calibrazione?")
+    print("\n- 1 -> SI\n- 2 -> NO")
+    delayType = int(input()) # = 1 delay, = 2 NO delay
+
 print("\nPRIMA DI PROCEDERE: \n - Collega i %d microfoni ai primi %d canali di input del device audio selezionato \n - Collega le %d sorgenti/altoparlanti ai primi %d canali di output del device audio selezionato" %(inputChannels, inputChannels, outputChannels, outputChannels))
 print("\nDopo aver collegato tutto, premi un tasto qualsiasi e digita invio per andare avanti...")
 input()
 
 print("\nScegli il metodo di misura: \n- 1 -> SineSweep\n- 2 -> MLS\n")
-recMethod = int(input())
+measureMethod = int(input())
 
-if recMethod != 1 and recMethod != 2 :
+if measureMethod != 1 and measureMethod != 2 :
    print("\nERRORE!\nScegli il metodo di misura: \n- 1 -> SineSweep\n- 2 -> MLS\n")
-   recMethod = int(input())
+   measureMethod = int(input())
+print("\nInserisci le posizioni note delle %d sorgenti (NB: le posizioni delle sorgenti/altoparlanti sono note, quelle dei microfoni incognite):" %(outputChannels))
+
+if cal_type == 1 :
+    knownPos = np.zeros((outputChannels,2))
+    for i in range(0,outputChannels) : 
+        print("\nPosizione X sorgente/altoparlante %d [m]:" %(i+1))
+        knownPos[i,0] = float(input())
+        print("\nPosizione Y sorgente/altoparlante %d [m]:" %(i+1))
+        knownPos[i,1] = float(input())
+elif cal_type == 2 :
+    knownPos = np.zeros((outputChannels,3))
+    for i in range(0,outputChannels) : 
+        print("\nPosizione X sorgente/altoparlante %d [m]:" %(i+1))
+        knownPos[i,0] = float(input())
+        print("\nPosizione Y sorgente/altoparlante %d [m]:" %(i+1))
+        knownPos[i,1] = float(input())
+        print("\nPosizione Z sorgente/altoparlante %d [m]:" %(i+1))
+        knownPos[i,2] = float(input())
 
 ######### INIZIO CODICE
