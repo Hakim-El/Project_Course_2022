8b48e0ae8de1a7c09a9decc1d2459c6899dffa13
Lorenzo <81576481+LorenzoLellini@users.noreply.github.com>
2022-04-27 16:17:02 +0200

    gui

--- 4_ProveGUI/ProveGUI2.py
+++ 4_ProveGUI/ProveGUI2.py
@@ -1,7 +1,11 @@
 from email.mime import audio
 import tkinter as tk
 import sounddevice as sd
+import scipy
+import numpy as np
+import os
 
+## FUNZIONE CHE NON SERVE PIU'
 def printDevices():
     #print (sd.query_devices())
     deviceWindow = tk.Tk()
@@ -18,7 +22,7 @@ def printDevices():
         tk.Label(frame, text=' - OUT  :  {}'.format(devices[counter]['max_output_channels'])).pack(side=tk.LEFT)
         frame.pack()
         counter += 1
-   
+########
 # CREA MAIN WINDOW
 mainWindow = tk.Tk()
 mainWindow.title("Automatic RIR Measurement System") # titolo
@@ -61,8 +65,45 @@ istructions1.place(x=10, y=140)
 istructions2 = tk.Label(mainWindow, text="Connect the selected number n of microphones to\n the first n input channels of the selected input device.\n\nConnect the selected number m of loudspeakers to\n the first m output channels of the selected output device.", fg='#36454f')
 istructions2.place(x=10, y=200)
 
+# Selezione tipo di misura
+InputDevicesList = ['SineSweep', 'MLS','PyRoomAcoustics simulation']
+variable = tk.StringVar(mainWindow)
+variable.set('Select the type of measure')
+opt2 = tk.OptionMenu(mainWindow, variable, *InputDevicesList)
+opt2.place(x=250, y=10)
+
+# Selezione Sampling Frequency
+InputDevicesList = [44100, 48000,96000]
+variable = tk.StringVar(mainWindow)
+variable.set('Select the Sampling Frequency [Hz]')
+opt2 = tk.OptionMenu(mainWindow, variable, *InputDevicesList)
+opt2.place(x=250, y=40)
+
+# Selezione tipo di calibrazione
+InputDevicesList = ['2D calibration', '3D calibration']
+variable = tk.StringVar(mainWindow)
+variable.set('Select the type of calibration')
+opt2 = tk.OptionMenu(mainWindow, variable, *InputDevicesList)
+opt2.place(x=550, y=10)
+
+# Delay o no Delay
+InputDevicesList = ['Delay estimation', 'NO Delay estimation']
+variable = tk.StringVar(mainWindow)
+variable.set('Delay estimation selection')
+opt2 = tk.OptionMenu(mainWindow, variable, *InputDevicesList)
+opt2.place(x=550, y=40)
+
+# Sound Speed estimation
+InputDevicesList = ['Set default value (343 [m/s])', 'Insert temperature in °C below']
+variable = tk.StringVar(mainWindow)
+variable.set('Sound Speed')
+opt2 = tk.OptionMenu(mainWindow, variable, *InputDevicesList)
+opt2.place(x=550, y=70)
+T = tk.Entry(mainWindow, width=5)
+T.place(x=550, y=100)
+
 # Start Measure Button
-#buttonStart = tk.Button(mainWindow, text="START MEASURE", command = , fg='#36454f') # Inserisci command per far partire misura
-#buttonStart.place(x=545, y=500)
+buttonStart = tk.Button(mainWindow, height=5, width=10, text="START MEASURE", fg='#36454f') # Inserisci command = funzione main tra text e fg per far partire misura
+buttonStart.place(x=600, y=600)
 
 mainWindow.mainloop()
